671aaeb5ab1a735e52ce81482d662f3ee12eac66
DavidL <locd011@cs.unm.edu>
2013-09-17 15:40:00 -0600

    Stupid copying error

--- CS351 Projects/src/com/putable/siteriter/locd011/Symbolable.java
+++ CS351 Projects/src/com/putable/siteriter/locd011/Symbolable.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public interface Symbolable {
 	public String getName();
 	public Symbolable getChoiceAt(int index);
-	public Token getTokenAt(int index);
+	public LazyToken getTokenAt(int index);
 	public int getTokenLength();
-	public String evaluate(Random r, Map<String, Symbol> symbolTable, Map<String, Integer> select);
+	public String evaluate(Random r, Map<String, Symbolable> symbolTable, Map<String, Integer> select);
 }
