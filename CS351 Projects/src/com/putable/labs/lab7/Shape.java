17be34abc5c83e045707e267cf24b17542c0043a
DavidL <locd011@cs.unm.edu>
2013-10-07 13:47:41 -0600

    Added lab 7 and implementation

---
+++ CS351 Projects/src/com/putable/labs/lab7/Shape.java
@@ -0,0 +1,12 @@
+package com.putable.labs.lab7;
+
+public enum Shape {
+	/**
+	 * The enum {@link Shape} defines the 3 shapes that this {@link ShapePanel}
+	 * can draw.
+	 * 
+	 * @author BKey
+	 * 
+	 */
+	SQUARE, CIRCLE, TRIANGLE;
+}
---
+++ CS351 Projects/src/com/putable/labs/lab7/ShapePanel.java
@@ -0,0 +1,149 @@
+package com.putable.labs.lab7;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ShapePanel extends JPanel{
+
+	private static final long serialVersionUID = -7450960793776661568L;
+	private List<Shape> shapes;
+	private int maxSize;
+	
+	private ShapePanel(List<Shape> shapes, int maxSize)
+	{
+		this.shapes = shapes;
+		this.maxSize = maxSize;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		Random r = new Random();
+		int center = getHeight()/2;
+		g.drawRect(maxSize/2, maxSize/2, 1, 1);
+		for(Shape shape : shapes)
+			shape.draw(g, r.nextInt(maxSize), center);
+	}
+	
+	/**
+	 * The enum {@link Shape} defines the 3 shapes that this {@link ShapePanel}
+	 * can draw.
+	 * 
+	 * @author BKey
+	 * 
+	 */
+	private enum Shape {
+		SQUARE(new ShapeGen()
+			{
+				@Override
+				public void drawShape(Graphics g, int size, int center) {
+					int halfLength = size/2;
+					int upper = center + halfLength;
+					int lower = center - halfLength;
+					int[] xpoints = {lower, upper, upper, lower};
+					int[] ypoints = {upper, upper, lower, lower};
+					Polygon square = new Polygon(xpoints, ypoints, 4);
+					g.setColor(Color.BLUE);
+					g.drawPolygon(square);
+				}
+			
+			}),
+		CIRCLE(new ShapeGen()
+			{
+				@Override
+				public void drawShape(Graphics g, int size, int center) {
+					g.setColor(Color.RED);
+					g.drawArc(center - size/2, center - size/2, size, size, 0, 360);
+				}
+				
+			}),
+		TRIANGLE(new ShapeGen()
+			{
+				@Override
+				public void drawShape(Graphics g, int size, int center) {
+					int[] xpoints = {center, center + size/2, center - size/2};
+					int ypos = (int)(size/Math.sqrt(3));
+					int yneg = (int)(size/(2*Math.sqrt(3)));
+					int[] ypoints = {center - ypos, center + yneg, center + yneg};
+					Polygon triangle = new Polygon(xpoints, ypoints, 3);
+					g.setColor(Color.GREEN);
+					g.drawPolygon(triangle);
+				}
+			});
+		
+		private ShapeGen generator;
+		
+		private Shape(ShapeGen generator){
+			this.generator = generator;
+		}
+		
+		private void draw(Graphics g, int size, int center)
+		{
+			generator.drawShape(g, size, center);
+		}
+		
+		private interface ShapeGen
+		{
+			public void drawShape(Graphics g, int size, int center);
+		}
+	}
+	
+	/**
+	 * The driver for this lab 7.
+	 * 
+	 * @param args
+	 *            is an array of {@code Strings} which hold the names of shapes
+	 *            to draw.
+	 */
+	public static void main(String[] args) {
+		List<Shape> shapesToDraw = new ArrayList<Shape>();
+		String currentShapeName = "";
+
+		if (args.length <= 0)
+			System.out
+					.println("Please provide arguments as to what shape(s) (Square, Circle or Triangle) you would like to draw.");
+		else {
+			// fill the list
+			for (int i = 0; i < args.length; i++) {
+				// can't switch on Strings in Java < 1.7... Gah.
+				currentShapeName = args[i];
+				if (currentShapeName.equalsIgnoreCase("Circle"))
+					shapesToDraw.add(Shape.CIRCLE);
+				else if (currentShapeName.equalsIgnoreCase("Square"))
+					shapesToDraw.add(Shape.SQUARE);
+				else if (currentShapeName.equalsIgnoreCase("Triangle"))
+					shapesToDraw.add(Shape.TRIANGLE);
+				else {
+					// I don't know what shape this is. Explode.
+					System.out
+							.println("This program only draws Squares, Circles or Triangles. Sorry.");
+					System.exit(0);
+				}
+			}
+
+			JFrame mainFrame = new JFrame("Shape Artist");
+			int maxSize = 500;
+			JPanel shapePanel = new ShapePanel(shapesToDraw, maxSize);
+
+			shapePanel.setPreferredSize(new Dimension(maxSize, maxSize));
+
+			// add the JPanel to the pane
+			mainFrame.getContentPane().add(shapePanel, BorderLayout.CENTER);
+
+			// clean up
+			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			mainFrame.pack();
+			mainFrame.setResizable(false);
+			mainFrame.setVisible(true);
+		}
+	}
+}
